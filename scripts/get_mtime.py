f47e69a9e929ec1b8ae8ce864221c28a23ca3538
Ethan Montgomery <ethan.montgomery.30@gmail.com>
2026-05-24 14:42:21 -0400

    Opt get_mtime.py into type checking

--- scripts/get_mtime.py
+++ scripts/get_mtime.py
@@ -9,11 +9,11 @@ import sys
 import time
 
 
-def main():
+def main() -> None:
     mtime = max(os.stat(filename).st_mtime for filename in sys.argv[1:])
     date = time.strftime("%Y-%m-%d", time.gmtime(mtime))
     print(f"date: {date}")
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
